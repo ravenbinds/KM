53e8ea6e28af9f56e675b326fa3492dbf21bcc14
ravenbinds <ravenbinds@gmail.com>
2021-04-16 18:40:56 +0530

    Adding project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Header';
+import Post from './components/Post';
+import SignIn from './components/signin/Signin'
+import Dashboard from './components/Dashboard/dashboard'
+class App extends Component {
+  render() {
+    return <div className="App">
+      {/* <Header /> */}
+      {/* <SignIn /> */}
+      <Dashboard />
+      <section className="App-main">
+        {/* <Post nickname="Chris" avatar="https://www.laravelnigeria.com/img/chris.jpg" caption="Moving the community!" image="https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg" />
+        <Post nickname="OG" avatar="https://www.laravelnigeria.com/img/chris.jpg" caption="Holding a mic" image="https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg" /> */}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        {/* more posts */}
+      </section>
+    </div>;
+  }
 }
-
 export default App;
---
+++ src/components/Dashboard/Deposits.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Title from './title';
+import Post from '../Post/index'
+import Grid from '@material-ui/core/Grid'
+function preventDefault(event) {
+    event.preventDefault();
+}
+
+const useStyles = makeStyles({
+    depositContext: {
+        flex: 1,
+    },
+});
+
+export default function Deposits() {
+    const classes = useStyles();
+    return (
+        <React.Fragment>
+            <Title>Posts</Title>
+            <Typography color="textSecondary" className={classes.depositContext}>
+                <Post nickname="Chris" avatar="https://www.laravelnigeria.com/img/chris.jpg" caption="Moving the community!" image="https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg" />
+                <Post nickname="OG" avatar="https://www.laravelnigeria.com/img/chris.jpg" caption="Holding a mic" image="https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg" />
+            </Typography>
+
+        </React.Fragment>
+    );
+}
