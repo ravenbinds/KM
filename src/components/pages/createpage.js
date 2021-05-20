1a04518b08811ef1c9e562436392515a5c40dc59
ravenbinds <ravenbinds@gmail.com>
2021-05-21 01:27:33 +0530

    Changes to content and support page

--- src/components/pages/createpage.js
+++ src/components/pages/createpage.js
@@ -7,6 +7,8 @@ import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
+import { Button, Checkbox, FormControlLabel, InputAdornment, TextField } from '@material-ui/core';
+import Supportreq from './supportreq';
 
 function TabPanel(props) {
     const { children, value, index, ...other } = props;
@@ -51,6 +53,15 @@ const useStyles = makeStyles((theme) => ({
         padding: theme.spacing(1),
 
     },
+    form: {
+        width: '100%',
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        maxWidth: '80px',
+        maxHeight: '43px',
+        margin: theme.spacing(3, 1, 2),
+    },
 }));
 
 export default function Createpages() {
@@ -84,7 +95,7 @@ export default function Createpages() {
                         aria-label="scrollable auto tabs example"
                     >
                         <Tab label="Create support task" {...a11yProps(0)} />
-                        <Tab label="View requests" {...a11yProps(1)} />
+                        <Tab label="View my requests" {...a11yProps(1)} />
                         <Tab label="Acknowledge support" {...a11yProps(2)} />
                         <Tab label="Track support projects" {...a11yProps(3)} />
 
@@ -92,8 +103,74 @@ export default function Createpages() {
                     </Tabs>
                 </AppBar>
                 <TabPanel value={value} index={0}>
-                    Item One
-      </TabPanel>
+                    <form className={classes.form} noValidate>
+                        <TextField
+
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="username"
+                            label="To"
+                            defaultValue="@username"
+                            helperText="Enter username"
+                            name="username"
+                            autoComplete="name"
+                            autoFocus
+
+                        ></TextField>
+                        <TextField
+
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            multiline
+                            fullWidth
+                            id="req"
+                            label="Request description"
+                            name="req"
+                            autoComplete="username"
+                            autoFocus
+                            rows={4}
+                        />
+                        <TextField
+
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="projectname"
+                            label="Project title"
+                            name="projectname"
+                            autoComplete="name"
+                            autoFocus
+
+                        />
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="Paid"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                        >
+                            Submit
+          </Button>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                        >
+                            Reset
+          </Button>
+
+                    </form>
+                </TabPanel>
                 <TabPanel value={value} index={1}>
                     Item Two
       </TabPanel>
@@ -101,8 +178,10 @@ export default function Createpages() {
                     Item Three
       </TabPanel>
                 <TabPanel value={value} index={3}>
-                    Item Four
-      </TabPanel>
+                    <Supportreq />
+                    <Supportreq />
+
+                </TabPanel>
 
             </div></div>
     );
