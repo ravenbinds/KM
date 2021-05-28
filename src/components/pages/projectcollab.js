fb7fe140662f895160a70927d406be10ea9b0f52
Khadija Mohamad Haneefa <diju19599@gmail.com>
2021-05-28 12:14:24 +0530

    Updated projectadopt.js and projectcollab.js

--- src/components/pages/projectcollab.js
+++ src/components/pages/projectcollab.js
@@ -1,6 +1,4 @@
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import man from "../../man.svg"
