acabd26b391633e6caf960c2716bfa100b1dc302
ravenbinds <ravenbinds@gmail.com>
2021-06-06 17:29:36 +0530

    comment file

---
+++ src/components/Comments.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Comments() {
+    return (
+        <div>
+            We created a React div component! WOO!
+        </div>
+    )
+}
+
+export default Comments
