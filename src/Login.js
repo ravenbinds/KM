4920ac1ac2c4b24409d7d0099f86a48207fe0c16
Khadija Mohamad Haneefa <diju19599@gmail.com>
2021-06-07 13:56:49 +0530

    fixed authorization
    note - error in db.collection in content.js
    commented out

--- src/Login.js
+++ src/Login.js
@@ -1,24 +1,70 @@
 import React, { useCallback, useContext } from "react";
+
+import Avatar from '@material-ui/core/Avatar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import {Formik, Form} from 'formik';
+import * as Yup from 'yup';
+import Textfield from './components/FormsUI/Textfield'
+import Button from './components/FormsUI/Button';
+import Checkbox from './components/FormsUI/Checkbox';
+
+
 import { withRouter, Redirect } from "react-router";
 import app from "./firebase";
 import { AuthContext } from "./contexts/AuthContext";
 
+const useStyles = makeStyles((theme) => ({
+  paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+  },
+  avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+  },
+  submit: {
+      margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+const INITIAL_FORM_STATE = {
+  email: '',
+  password: '',
+  // rememberMe: false,
+};
+
+const FORM_VALIDATION = Yup.object().shape({
+  email: Yup.string().email('Invalid email').required('Required'),
+  password: Yup.string().label('Password').required().min(6, 'Seems a bit short...').max(10, 'We prefer insecure system, try a shorter password.'),
+});
+
+
+
 const Login = ({ history }) => {
-  const handleLogin = useCallback(
-    async event => {
-      event.preventDefault();
-      const { email, password } = event.target.elements;
-      try {
-        await app
-          .auth()
-          .signInWithEmailAndPassword(email.value, password.value);
-        history.push("/");
-      } catch (error) {
-        alert(error);
-      }
-    },
-    [history]
-  );
+  const classes = useStyles();
+
+  const handleLogin = useCallback(async values => {
+      // event.preventDefault();
+      const email = values["email"]
+      const password = values["password"]
+      // alert(event.elements)
+      try { await app.auth().signInWithEmailAndPassword(email, password);
+            history.push("/");
+          } 
+      catch (error) { alert(error);}
+    }, [history] );
 
   const { currentUser } = useContext(AuthContext);
 
@@ -26,21 +72,55 @@ const Login = ({ history }) => {
     return <Redirect to="/" />;
   }
 
+  
+
   return (
-    <div>
-      <h1>Log in</h1>
-      <form onSubmit={handleLogin}>
-        <label>
-          Email
-          <input name="email" type="email" placeholder="Email" />
-        </label>
-        <label>
-          Password
-          <input name="password" type="password" placeholder="Password" />
-        </label>
-        <button type="submit">Log in</button>
-      </form>
-    </div>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+            <div className={classes.paper}>
+                <Formik
+                    initialValues={{...INITIAL_FORM_STATE}}
+                    validationSchema={FORM_VALIDATION}
+                    onSubmit = {handleLogin}
+                    handleSubmit = {handleLogin}
+                >
+                    <Form>                    
+                        <Grid container spacing={1}>
+                            <Grid item xs={12} align='center'>
+                                <Avatar className={classes.avatar}> <LockOutlinedIcon /> </Avatar>
+                            </Grid>
+                            <Grid item xs={12} align='center'>
+                                <Typography component="h1" variant="h5">Sign in</Typography>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Textfield label="Email Address" name="email"/>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Textfield name="password" label="Password" type='password' />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Checkbox name="rememberMe" label="Remember Me" />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Button type='submit'> Sign In </Button>
+                            </Grid>
+                            <Grid container>
+                                <Grid item xs>
+                                    <Link href="#" variant="body2">
+                                        Forgot password?
+                                    </Link>
+                                </Grid>
+                                <Grid item>
+                                    <Link href='/SignUp' variant="body2">
+                                        {"Don't have an account? Sign Up"}
+                                    </Link>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Form>
+                </Formik>
+            </div>
+        </Container>
   );
 };
 
--- src/components/dash.js
+++ src/components/dash.js
@@ -1,41 +1,45 @@
-import React from 'react'
-import { withRouter } from 'react-router-dom';
-import { BrowserRouter as Router, Route, Switch, Link, Redirect } from 'react-router-dom';
-import Content from './Content';
-import Leftbar from './Leftbar';
-import Rightbar from './Rightbar';
-import errorpage from './404';
-import Huntingground from "./pages/Huntingground"
-import Myprofile from "./Myprofile"
-import Notifications from "./Notifications"
-import Settings from "./settings"
-import Projectcollab from './pages/projectcollab';
-import Projectadopt from './pages/projectadopt';
-import Projectpage from './pages/Projectpage';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link, Redirect, } from "react-router-dom";
+import Content from "./Content";
+import Leftbar from "./Leftbar";
+import Rightbar from "./Rightbar";
+import Huntingground from "./pages/Huntingground";
+import Myprofile from "./Myprofile";
+import Notifications from "./Notifications";
+import Settings from "./settings";
+import Projectcollab from "./pages/projectcollab";
+import Projectadopt from "./pages/projectadopt";
+import Projectpage from "./pages/Projectpage";
+import errorpage from "./404";
+import create from "./pages/createpage";
+// import Dashboard from './Dashboard/dashboard'
+// import Register from './Actions/registration';
+
 function Dash() {
-    return (
-        <div className="App">
-            <div className="wrapper">
-
-                <Router>
-                    <Leftbar />
-                    <Switch>
-                        <Route exact path="/" component={Content} />
-                        <Route exact path="/Myprofile" component={Myprofile} />
-                        <Route exact path="/Projectpage" component={Projectpage} />
-                        <Route exact path="/Huntingground/projectcollab" component={Projectcollab} />
-                        <Route exact path="/Huntingground/projectadopt" component={Projectadopt} />
-                        <Route exact path="/Huntingground" component={Huntingground} />
-                        <Route exact path="/Notifications" component={Notifications} />
-                        <Route exact path="/Settings" component={Settings} />
-                        <Route component={errorpage} />
-
-                    </Switch>
-                    <Rightbar />
-                </Router>
-
-            </div></div>
-    )
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <Router>
+          <Leftbar />
+          <Switch>
+            <Route exact path="/" component={Content} />
+            <Route exact path="/createpages" component={create} />
+            <Route exact path="/Content" component={Content} />
+            <Route exact path="/Myprofile" component={Myprofile} />
+            <Route exact path="/Projectpage" component={Projectpage} />
+            <Route exact path="/Huntingground/projectcollab" component={Projectcollab}/>
+            <Route exact path="/Huntingground/projectadopt" component={Projectadopt}/>
+            <Route exact path="/Huntingground" component={Huntingground} />
+            <Route exact path="/Notifications" component={Notifications} />
+            <Route exact path="/Settings" component={Settings} />
+            <Route component={errorpage} />
+          </Switch>
+          <Rightbar />
+        </Router>
+      </div>
+    </div>
+  );
 }
 
-export default withRouter(Dash);
+// export default withRouter(Dash);
+export default Dash;
