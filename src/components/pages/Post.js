67662938a5be2ceffc796f4300ee92f9d526beb7
Krishnaja R. Nair <krishnajanair0@gmail.com>
2021-06-12 00:07:16 +0530

    Update Post.js

--- src/components/pages/Post.js
+++ src/components/pages/Post.js
@@ -2,7 +2,7 @@ import React, { forwardRef } from "react";
 import Grid from '@material-ui/core/Grid';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
-import { Avatar, Checkbox, FormControlLabel, IconButton, makeStyles, Typography } from "@material-ui/core";
+import { Avatar, Checkbox, FormControlLabel, IconButton, makeStyles, Typography, Divider } from "@material-ui/core";
 import { Comment, Favorite, FavoriteBorder } from "@material-ui/icons";
 const useStyles = makeStyles((theme) => ({
     large: {
@@ -12,6 +12,15 @@ const useStyles = makeStyles((theme) => ({
     },
     Grid: {
         padding: theme.spacing(1),
+
+    },
+    Grid1: {
+        boxShadow: '0 2px 0px 0 rgba(0, 0, 0, 0.1),0 1px 0px 0 rgba(121, 121, 121, 0.19)',
+
+    },
+    Grid2: {
+        boxShadow: '0px 0px 4px 0 rgba(0, 0, 0, 0.2)',
+        borderRadius: '8px',
 
     },
     Box1: {
@@ -45,7 +54,7 @@ const Post = forwardRef(
 
         return (
             <Grid container className={classes.Box} ref={ref}>
-                <Grid container direction="row" justify="flex-start" alignItems="center"  >
+                <Grid container direction="row" justify="flex-start" alignItems="center" className={classes.Grid1} >
                     <Grid item className={classes.Box1}>
                         <Avatar src={avatar} className={classes.large} />
                     </Grid>
@@ -54,28 +63,29 @@ const Post = forwardRef(
                             {nickname}
                         </Typography>
                     </Grid>
+
                 </Grid>
+                <Grid container className={classes.Grid}>
+                    <Grid item className={classes.Grid}>
+
+                        {caption}
+                    </Grid>   </Grid>
                 <Grid container justify="center" className="Post-image">
                     <Grid item className={classes.Grid}>
                         <img alt={""} src={image} />
                     </Grid>
                 </Grid>
-                <Grid container className={classes.Grid}>
-                    {caption}
-                </Grid>
-                <Grid container justify="space-evenly" >
+
+                <Grid container justify="space-around" className={classes.Grid2}>
                     <FormControlLabel control={<Checkbox icon={<FavoriteBorder />} checkedIcon={<Favorite color="primary" />} name="checkedH" />}
                     />
-
                     <IconButton aria-label="share">
                         <ShareIcon />
                     </IconButton>
                     <IconButton aria-label="comment">
                         <Comment />
                     </IconButton></Grid>
-                <Grid >
-                    Comments
-                </Grid>
+
             </Grid>
         );
     }
