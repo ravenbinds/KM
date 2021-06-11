baee7eaac3c82c3013c5b1a86bf147f950384942
Khadija Mohamad Haneefa <diju19599@gmail.com>
2021-06-11 23:14:10 +0530

    Update HGPosts.js

--- src/components/HGPosts.js
+++ src/components/HGPosts.js
@@ -1,3 +1,4 @@
+import {useState} from 'react'
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
@@ -30,7 +31,7 @@ const useStyles = makeStyles((theme) => ({
     Box: {
         display: 'flex',
         alignItems: 'flex-start ',
-        padding: theme.spacing(1),
+        padding: theme.spacing(2),
         flexDirection: 'column',
         background: '#FFFFFF',
         border: '1px solid #985DFF',
@@ -62,19 +63,130 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function HGPosts(props) {
+
     const { username, title, description, tag, category, seeklist, button, buttonText } = props;
     const classes = useStyles();
-    let isProjectCollab = false
-    let isFreelance = false
+    let isProjectCollab = false;
+    let isFreelance = false;
+    let descriptioncomponentsize = 12;
 
     if (category === '1') {
-        isProjectCollab = true
+        isProjectCollab = true;
     }
     if (category === '3') {
         isFreelance = true
     }
+    if(seeklist) {
+        descriptioncomponentsize = 8;
+    }
     return (
-        <Grid container justify="flex-start" alignItems="flex-start" className={classes.Grid}>
+        <Grid container direction='column' justify="flex-start" className={classes.Box} spacing={1}>
+                <Grid item xs={12}>
+                    <Typography color="primary" variant="h5" align="left" >
+                        {title}
+                    </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    <Grid container direction="row" spacing={1} alignItems='center' justify='flex-start'>
+                        <Grid item xs={descriptioncomponentsize}>
+                            <Typography variant="body1">
+                                {description}
+                            </Typography>  
+                        </Grid> 
+                        {isProjectCollab && seeklist && 
+                            <Grid item xs={4}>
+                                <Grid container direction="column" justify="flex-start" alignItems="flex-start" >
+                                    <Grid item xs={12} className={classes.Box} >
+                                        <Grid container direction="column" justify="flex-start" alignItems="flex-start" >
+                                            <Grid item xs={12} >
+                                                <Typography>
+                                                    Looking for:
+                                                </Typography>
+                                            </Grid>
+                                            <Grid item xs={12} >
+                                                {seeklist && seeklist.map(item => (
+                                                            <Grid item xs={12}>
+                                                                <Typography variant="body1">+{item}</Typography>
+                                                            </Grid>
+                                                    ))
+                                                }
+                                            </Grid>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        }
+                        {isFreelance && seeklist && 
+                            <Grid item xs={4}>
+                                <Grid container direction="column" justify="flex-start" alignItems="flex-start" >
+                                    <Grid item xs={12} className={classes.Box} >
+                                        <Grid container direction="column" justify="flex-start" alignItems="flex-start" >
+                                            <Grid item xs={12} >
+                                                <Typography>
+                                                    Looking for:
+                                                </Typography>
+                                            </Grid>
+                                            <Grid item xs={12} >
+                                                {seeklist && seeklist.map(item => (
+                                                            <Grid item xs={12} className={classes.Grid}>
+                                                                <Typography variant="body1">+{item}</Typography>
+                                                            </Grid>
+                                                    ))
+                                                }
+                                            </Grid>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        }
+                    </Grid>
+                </Grid>              
+                <Grid item xs={12} style={{background: 'pink'}}>
+                    <Grid container direction='row' justify='space-between'>
+                        <Grid item>
+                            <Grid container direction='column'>
+                                <Grid item>
+                                    <Typography align="left" color="primary" variant="body1">
+                                        {tag}
+                                    </Typography>
+                                </Grid>
+                                <Grid item >
+                                    <Grid container direction="row" justify="flex-start" spacing={1} alignItems='center'>
+                                        <Grid item>
+                                            <Avatar alt="Remy Sharp" className={classes.large} />
+                                        </Grid>
+                                        <Grid item>
+                                            <Typography variant="body1">
+                                                {username}
+                                            </Typography>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Grid item>
+                            <Grid container direction="row" justify="space-between">
+                                <Grid item className={classes.Box1}>
+                                    <SimpleModal title={"title"} button="More info" variant='outlined' color="textSecondary" />
+                                </Grid>  
+                                <Grid item className={classes.Box1}>
+                                    <SimpleModal title={"title"} button={buttonText} variant='outlined' />
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+    );
+}
+
+export default HGPosts
+
+HGPosts.defaultProps = {
+    username: 'Username', title: 'Project Name', description: 'Description', tag: 'DEFAULTTAG', category: '0', seeklist: null, button: '#', buttonText: "Apply"
+}
+
+{/* <Grid container justify="flex-start" alignItems="flex-start" className={classes.Grid}>
             <Grid item xs={12} className={classes.Box}>
                 <Grid item xs={12} className={classes.Box1}>
                     <Typography color="primary" variant="h5" align="left" >
@@ -82,99 +194,63 @@ function HGPosts(props) {
                     </Typography>
                 </Grid>
 
-                <Grid
-                    container
-                    direction="row"
-                    className={classes.Grid}
-                >
+                <Grid container direction="row" className={classes.Grid} >
                     <Grid item xs={8} className={classes.Box1}>
                         <Typography variant="body1">
                             {description}
-                        </Typography>  </Grid>
-
-
-
+                        </Typography>  
+                    </Grid>
                     <Grid item xs={4}  >
-
                         {isProjectCollab && seeklist && <div>
-                            <Grid
-                                container
-                                direction="column"
-                                justify="flex-start"
-                                alignItems="flex-start"
-
-                            >
+                            <Grid container direction="column" justify="flex-start" alignItems="flex-start" >
                                 <Grid item xs={12} className={classes.Box} >
-                                    <Grid container direction="column"
-                                        justify="flex-start"
-                                        alignItems="flex-start" >
+                                    <Grid container direction="column" justify="flex-start" alignItems="flex-start" >
                                         <Grid item xs={12} >
                                             <Typography>
                                                 Looking for:
-                                            </Typography></Grid>
+                                            </Typography>
+                                        </Grid>
                                         <Grid item xs={12} >
-
-                                            {seeklist &&
-                                                seeklist.map(item => (
-                                                    <div>
+                                            {seeklist && seeklist.map(item => (
                                                         <Grid item xs={12} className={classes.Grid}>
-                                                            <Typography variant="body1">+{item}</Typography></Grid>
-                                                    </div>
-
+                                                            <Typography variant="body1">+{item}</Typography>
+                                                        </Grid>
                                                 ))
-                                            }</Grid>
-
+                                            }
+                                        </Grid>
                                     </Grid>
                                 </Grid>
-
                             </Grid>
-
                         </div>
-
                         }
-
                         {isFreelance && seeklist && <div>
-                            <Grid
-                                container
-                                direction="column"
-                                justify="flex-start"
-                                alignItems="flex-start"
-
-                            >
+                            <Grid container direction="column" justify="flex-start" alignItems="flex-start" >
                                 <Grid item xs={12} className={classes.Box} >
-                                    <Grid container direction="column"
-                                        justify="flex-start"
-                                        alignItems="flex-start" >
+                                    <Grid container direction="column" justify="flex-start" alignItems="flex-start" >
                                         <Grid item xs={12} >
                                             <Typography>
                                                 Looking for:
-                                            </Typography></Grid>
+                                            </Typography>
+                                        </Grid>
                                         <Grid item xs={12} >
-
-                                            {seeklist &&
-                                                seeklist.map(item => (
-                                                    <div>
+                                            {seeklist && seeklist.map(item => (
                                                         <Grid item xs={12} className={classes.Grid}>
-                                                            <Typography variant="body1">+{item}</Typography></Grid>
-                                                    </div>
-
+                                                            <Typography variant="body1">+{item}</Typography>
+                                                        </Grid>
                                                 ))
-                                            }</Grid>
-
+                                            }
+                                        </Grid>
                                     </Grid>
                                 </Grid>
-
                             </Grid>
-
                         </div>
-                        }
+                    }
                     </Grid>
                 </Grid>
                 <Grid item xs={12} >
                     <Typography align="left" color="primary" variant="body1">
                         {tag}
                     </Typography>
-
                 </Grid>
                 <Grid container direction="row" justify="space-between" >
                     <Grid item>
@@ -201,13 +277,4 @@ function HGPosts(props) {
                 </Grid>
             </Grid>
 
-        </Grid>
-
-    );
-}
-
-export default HGPosts
-
-HGPosts.defaultProps = {
-    username: 'Username', title: 'Project Name', description: 'Description', tag: 'DEFAULTTAG', category: '0', seeklist: null, button: '#', buttonText: "Apply"
-}
+        </Grid> */}
