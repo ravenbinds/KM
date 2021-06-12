610112fbf74c2b7a073100a34d71339976e26f86
Krishnaja R. Nair <krishnajanair0@gmail.com>
2021-06-12 12:27:45 +0530

    Project page update

--- src/components/pages/supportreq.js
+++ src/components/pages/supportreq.js
@@ -7,7 +7,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import { Avatar, CardHeader } from '@material-ui/core';
+import { Avatar, CardHeader, Grid } from '@material-ui/core';
 
 const useStyles = makeStyles({
     root: {
@@ -20,35 +20,36 @@ function Supportreq() {
     const classes = useStyles();
 
     return (
-        <Card className={classes.root}>
-            <CardHeader
-                avatar={
-                    <Avatar aria-label="recipe" className={classes.avatar}>
-                        R
-          </Avatar>
-                }
+        <div className="Contents">
+            <Card className={classes.root}>
+                <CardHeader
+                    avatar={
+                        <Avatar aria-label="recipe" className={classes.avatar}>
+                            R
+                        </Avatar>
+                    }
 
-                title="Hana Banana"
-                subheader="September 14, 2016"
-            />
-            <CardActionArea>
-                <CardContent>
-
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        Hi, We are absolute fans of your latest illustration works in project #howwellcanitbedone.
-                        Will you help us in the logo design for our latest project Choco Chip Cookies?
-            </Typography>
-                </CardContent>
-            </CardActionArea>
-            <CardActions>
-                <Button variant="outlined" size="small" color="primary">
-                    Accept
-          </Button>
-                <Button variant="outlined" size="small" color="primary">
-                    Cancel
-          </Button>
-            </CardActions>
-        </Card>
+                    title="Hana Banana"
+                    subheader="September 14, 2016"
+                />
+                <CardActionArea>
+                    <CardContent>
+                        <Grid item xs={12}>
+                            <Typography variant="body2" color="textSecondary" component="p">
+                                Hi, We are absolute fans of your latest illustration works in project #howwellcanitbedone.
+                                Will you help us in the logo design for our latest project Choco Chip Cookies?
+                            </Typography></Grid>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button variant="outlined" size="small" color="primary">
+                        Accept
+                    </Button>
+                    <Button variant="outlined" size="small" color="primary">
+                        Cancel
+                    </Button>
+                </CardActions>
+            </Card></div>
     )
 }
 
